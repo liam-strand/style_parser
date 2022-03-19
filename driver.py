51783caf048ee0fac98a2c978d674aaff3d82479
liam-strand <Liam.Strand@tufts.edu>
2022-03-18 22:41:19 -0400

    add cl helper

--- driver.py
+++ driver.py
@@ -1,4 +1,5 @@
 from check_length import generate_report
+import sys
 
 if __name__ == "__main__":
-    print(generate_report("testfiles/acolli10.1.cpp", 30))
+    print(generate_report(sys.argv[1], 30))
