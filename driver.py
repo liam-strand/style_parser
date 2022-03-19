ef84802596a4c2fe8c84faaa456a80f86be0d55e
liam-strand <Liam.Strand@tufts.edu>
2022-03-18 22:45:06 -0400

    formatting

--- driver.py
+++ driver.py
@@ -2,4 +2,4 @@ from check_length import generate_report
 import sys
 
 if __name__ == "__main__":
-    print(generate_report(sys.argv[1], 30))
+    print(f"{sys.argv[1].split('/')[-1].split('.')[0]:8} {generate_report(sys.argv[1], 30)[-1]:>5.1f}")
