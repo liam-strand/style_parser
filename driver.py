6d8dee8bbaec803382ea2bad5b252db719aa7e3c
liam-strand <Liam.Strand@tufts.edu>
2022-03-19 00:36:32 -0700

    nested files

--- driver.py
+++ driver.py
@@ -1,5 +1,23 @@
 from check_length import generate_report
 import sys
+import os.path
+
+def main():
+    
+    files = get_files(sys.argv[1])
+
+    for file in files:
+        print(f"{file.split('/')[-1].split('.')[0]:8} {generate_report(file, 30)[-1]:>5.1f}")
+    
+def get_files(start_path: str) -> list:
+    files = []
+
+    for dirpath, _, filenames in os.walk(start_path):
+        for filename in [f for f in filenames if f.endswith(".cpp")]:
+            files.append(os.path.join(dirpath, filename))
+
+    return files
 
 if __name__ == "__main__":
-    print(f"{sys.argv[1].split('/')[-1].split('.')[0]:8} {generate_report(sys.argv[1], 30)[-1]:>5.1f}")
+    main()
+
