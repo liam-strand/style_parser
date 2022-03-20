51ae2821391bdf5a54922e7044ea6a9bb1342c4c
liam-strand <Liam.Strand@tufts.edu>
2022-03-20 11:00:22 -0700

    merge

--- driver.py
+++ driver.py
@@ -7,14 +7,10 @@ def main():
     files = get_files(sys.argv[1])
 
     for file in files:
-<<<<<<< HEAD
-        print(f"{file.split('/')[-2].split('.')[0]:8} {generate_report(file, 30)[-1]:>5.1f}")
-=======
         print(f"{file.split('/')[-1].split('.')[0]:8}")
         ll, ls, ld, ds = generate_report(file)
         print(f"{ll:>2} {ls:>5.1f} {ld:>2} {ds:>5.1f}")
 
->>>>>>> 7c0738041392912caddb2376c3d8ec3ebe2ad713
     
 def get_files(start_path: str) -> list:
     files = []
